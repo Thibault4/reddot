df3aab75a6dd61cd624ddd31665f99b607310af9
teiva.francis <teiva.francis@nolonely.fr>
2024-05-14 16:53:09 +0200

    Railskutenn

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,9 +5,13 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if params[:is_seller] == "on"
+      @user.build_seller
+    end
 
     if @user.save
       redirect_to root_path, notice: "Inscription réussie !"
+      session[:user_id] = @user.id
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,6 +20,7 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation, :is_seller)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'cart_items/create'
+  get 'cart_items/update'
+  get 'cart_items/destroy'
+  get 'orders/create'
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+  root 'products#index'
   resources :registrations
   resources :sessions, only: [:new, :create]
   resources :posts do
@@ -10,6 +21,11 @@ Rails.application.routes.draw do
   end
   resources :comments
   resources :users
+  resources :products
+  resources :buyers
+  resources :sellers
+  resources :cart
+  resources :cart_items
   delete 'logout' => 'sessions#destroy', as: :logout
   get "up" => "rails/health#show", as: :rails_health_check
 end
